97b103a1c4bc53f1dc807a10e6e902c4774bd1b6
Luigi Timossi <luigi.timossi.iic00@gmail.com>
2022-04-28 17:01:17 +0200

    primo xml

--- app/src/main/java/com/example/poliquiz/MainActivity.java
+++ app/src/main/java/com/example/poliquiz/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.poliquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private final String[] PERMISSIONS = new String[] {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.RECORD_AUDIO
+    };
+
+    private final String TAG = "MainActivity";
+    private Button bttStart = null;
+    private Button bttInfo = null;
+    private TextView Record = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bttStart = findViewById(R.id.bttStart);
+        bttInfo = findViewById(R.id.bttInfo);
+        Record = findViewById(R.id.tvRecord);
+
+        if (!hasPermissions(MainActivity.this,PERMISSIONS)) {
+            ActivityCompat.requestPermissions(MainActivity.this,PERMISSIONS,1);
+        }
+
+
+
     }
+
+
+    private boolean hasPermissions(Context context, String... PERMISSIONS) {
+        if (context != null && PERMISSIONS != null)
+            for (String permission: PERMISSIONS)
+                if (ActivityCompat.checkSelfPermission(context,permission) != PackageManager.PERMISSION_GRANTED)
+                    return false;
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);//////////////da controllare
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i("TAG", "permessi approvati");
+
+
+                } else {
+                    Log.i("TAG", "permission denied by user");
+                }
+                return;
+            }
+        }
+    }
+
 }
